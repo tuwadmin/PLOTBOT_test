a8df2bedf14f6d58c0f7c5e1817ba099371979f8
Abdullah-python <Abdullah55980@gmail.com>
2023-10-31 15:44:58 +0500

    Plotbot firmware

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,76 @@
 #include <Arduino.h>
 
-//Wifi Manager
+//-----------------------------------------------------------------Wifi Manager
 #include <WiFiManager.h>
 #define CAPTIVE_PORTAL_TIMEOUT 30
 
-//PH sensor
+bool runWiFiManager() {
+  /*
+     * Initialize WiFi and start captive portal to set connection credentials
+     */
+  WiFi.setHostname("PLOTBOT");
+  WiFi.mode(WIFI_AP);  // explicitly set mode, esp defaults to STA+AP
+  WiFi.enableAP(true);
+
+  WiFiManager wifiManager;
+  //wifiManager.setDebugOutput(true);
+  // wifiManager.debugPlatformInfo();
+  wifiManager.setTitle("PlotBot Configuration Portal");
+  wifiManager.setParamsPage(true);
+  //  const char *bufferStr = R"(
+
+  //   <!-- INPUT SELECT -->
+  //   <br/>
+  //   <label for='input_select'>INPUT 1</label>
+  //   <select name="input_select" id="input_select" class="button">
+  //   <option value="0">Analog </option>
+  //   <option value="1" selected>Digital</option>
+  //   </select>
+  //   )";
+
+  //   WiFiManagerParameter custom_html_inputs(bufferStr);
+  //   wifiManager.addParameter(&custom_html_inputs);
+  wifiManager.setSaveParamsCallback([&wifiManager]() {
+    // inputs(*DI1);
+  });
+
+  wifiManager.setDarkMode(true);
+
+  //wifiManager.setConfigPortalTimeout(CAPATITIVE_PORTAL_TIMEOUT / 1000); //if nobody logs in to the portal, continue after timeout
+  wifiManager.setTimeout(CAPTIVE_PORTAL_TIMEOUT);  //if nobody logs in to the portal, continue after timeout
+  wifiManager.setConnectTimeout(CAPTIVE_PORTAL_TIMEOUT);
+  //wifiManager.setSaveConnect(true);
+  wifiManager.setAPClientCheck(true);  // avoid timeout if client connected to softap
+  Serial.println("[main] Start capatitive portal");
+
+  if (wifiManager.startConfigPortal("PLOTBOT", "246810")) {
+    return true;
+  } else {
+    return wifiManager.autoConnect("PLOTBOT", "246810");
+  }
+}
+
+
+//-----------------------------------------------------------------DS18B20
+#include <OneWire.h>
+#include <DallasTemperature.h>
+const int oneWireBus = 13;            // GPIO where the DS18B20 is connected to
+OneWire oneWire(oneWireBus);          // Setup a oneWire instance to communicate with any OneWire devices
+DallasTemperature sensors(&oneWire);  // Pass our oneWire reference to Dallas Temperature sensor
+float temperatureC, temperatureF;
+
+float DS18B20_temp() {
+  sensors.requestTemperatures();
+  temperatureC = sensors.getTempCByIndex(0);
+  temperatureF = sensors.getTempFByIndex(0);
+  Serial.print(temperatureC);
+  Serial.println("ºC");
+
+  return temperatureC;
+}
+
+
+//-----------------------------------------------------------------PH sensor
 #include <Adafruit_ADS1X15.h>
 Adafruit_ADS1115 ads; /* Use this for the 16-bit version */
 unsigned long int avgValue;
@@ -13,26 +79,56 @@ float calibration_value = 21.34;  //20.24 - 0.7;  //21.34 - 0.7
 int16_t ph_adc;
 float ph_volt, phValue;
 
-//EC sensor
+float PH_value() {
+  ph_volt = ads.computeVolts(ph_adc);
+  Serial.print("PH raw: ");
+  Serial.print(ph_adc);
+
+  Serial.print("PH Voltage = ");
+  Serial.print(ph_volt, 4);
+  Serial.println("V");
+
+  phValue = (-5.70 * ph_volt) + calibration_value;
+  Serial.print("pH =");
+  Serial.println(phValue);
+
+  return phValue;
+}
+
+
+//-----------------------------------------------------------------EC sensor
 #include "DFRobot_ESP_EC.h"
 #include "EEPROM.h"
 DFRobot_ESP_EC ec;
 int16_t ec_adc;
 float ec_volt, sub_temp, ecValue;
 
-//DS18B20
-#include <OneWire.h>
-#include <DallasTemperature.h>
-const int oneWireBus = 13;            // GPIO where the DS18B20 is connected to
-OneWire oneWire(oneWireBus);          // Setup a oneWire instance to communicate with any OneWire devices
-DallasTemperature sensors(&oneWire);  // Pass our oneWire reference to Dallas Temperature sensor
-float temperatureC, temperatureF;
+float EC_value() {
+  ec_adc = ads.readADC_SingleEnded(0);
+  Serial.print("EC raw: ");
+  Serial.println(ec_adc);
 
-//SCD-30 CO2 Sensor
+  ec_volt = ads.computeVolts(ec_adc);
+  Serial.print("EC voltage:");
+  Serial.println(ec_volt, 4);
+
+  sub_temp = DS18B20_temp();
+
+  ecValue = ec.readEC(ec_volt, sub_temp);  // convert voltage to EC with temperature compensation
+  Serial.print("EC:");
+  Serial.print(ecValue, 4);
+  Serial.println("ms/cm");
+
+  return ecValue;
+}
+
+
+//-----------------------------------------------------------------SCD-30 CO2 Sensor
 #include <Adafruit_SCD30.h>
 Adafruit_SCD30 scd30;
 
-//HTTP server
+
+//-----------------------------------------------------------------HTTP server
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <WiFiClientSecure.h>
@@ -43,8 +139,9 @@ unsigned long timerDelay = 5000;
 WiFiClientSecure client;
 
 
-//General Variables
+//-----------------------------------------------------------------General Variables
 float EC_reading, PH_reading, Sub_temp_reading, Temp_reading, Hum_reading, CO2_reading;
+
 
 void setup() {
  Serial.begin(115200);
